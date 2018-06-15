aa50aa334995ab821dbf769c85ce62f1bdd80e62
Vishnu Y S <ysv8152@gmail.com>
2018-06-15 15:29:45 +0530

    FEAT: finish resume section and removed testimonials and portfolio section

--- newDesign/js/custom.js
+++ newDesign/js/custom.js
@@ -1,33 +1,7 @@
-/*
-  * @package 
-  * @subpackage template name HTML
-  * 
-  * Template Scripts
-  * Created by themeturn
-  
-  1. Fixed header
-  2. Site search
-  3. Main slideshow
-  4. Owl Carousel
-      a. Testimonial
-      b. Clients
-      c. Team
-  5. Back to top
-  6. Skills
-  7. BX slider
-      a. Blog Slider
-      b. Portfolio item slider
-  8. Isotope
-  9. Animation (wow)
-  10. Flickr
-  
-*/
-
-
 jQuery(function($) {
-  "use strict";
+    "use strict";
 
-   $('.navigation').singlePageNav({
+    $('.navigation').singlePageNav({
         currentClass: 'active',
         changeHash: true,
         scrollSpeed: 750,
@@ -38,21 +12,77 @@ jQuery(function($) {
     });
 
     $.noConflict();
-     $('.nav a').on('click', function(){ 
+    $('.nav a').on('click', function(){ 
         if($('.navbar-toggle').css('display') !='none'){
             $(".navbar-toggle").trigger( "click" );
         }
     });
 
+    // prettyphoto
+    $("a[data-rel^='prettyPhoto']").prettyPhoto();
 
- 
-// prettyphoto
+    function getAge(date_1, date_2)
+    {
 
- $("a[data-rel^='prettyPhoto']").prettyPhoto();
- 
-    
- 
-
+        //convert to UTC
+        var date2_UTC = new Date(Date.UTC(date_2.getUTCFullYear(), date_2.getUTCMonth(), date_2.getUTCDate()));
+        var date1_UTC = new Date(Date.UTC(date_1.getUTCFullYear(), date_1.getUTCMonth(), date_1.getUTCDate()));
 
 
+        var yAppendix, mAppendix, dAppendix;
+
+
+        //--------------------------------------------------------------
+        var days = date2_UTC.getDate() - date1_UTC.getDate();
+        if (days < 0)
+        {
+
+            date2_UTC.setMonth(date2_UTC.getMonth() - 1);
+            days += DaysInMonth(date2_UTC);
+        }
+        //--------------------------------------------------------------
+        var months = date2_UTC.getMonth() - date1_UTC.getMonth();
+        if (months < 0)
+        {
+            date2_UTC.setFullYear(date2_UTC.getFullYear() - 1);
+            months += 12;
+        }
+        //--------------------------------------------------------------
+        var years = date2_UTC.getFullYear() - date1_UTC.getFullYear();
+
+
+
+
+        if (years > 1) yAppendix = " years";
+        else yAppendix = " year";
+        if (months > 1) mAppendix = " months";
+        else mAppendix = " month";
+        if (days > 1) dAppendix = " days";
+        else dAppendix = " day";
+
+        var final = []; 
+        if (years != 0)
+          final.push(years + yAppendix)
+        if (months != 0)        
+          final.push(months + mAppendix)
+        if(days !=0)
+          final.push(days + dAppendix)
+
+        return final.join(', ');
+    }
+
+
+    function DaysInMonth(date2_UTC)
+    {
+        var monthStart = new Date(date2_UTC.getFullYear(), date2_UTC.getMonth(), 1);
+        var monthEnd = new Date(date2_UTC.getFullYear(), date2_UTC.getMonth() + 1, 1);
+        var monthLength = (monthEnd - monthStart) / (1000 * 60 * 60 * 24);
+        return monthLength;
+    }
+
+    var start_date = new Date(2017, 6, 1);
+    var current_date = new Date();
+    var diff = getAge(start_date, current_date);
+
+    $('#resume-details > div > div:nth-child(3) > div:nth-child(3) > div > div:nth-child(1) > div').text(diff);
 });
